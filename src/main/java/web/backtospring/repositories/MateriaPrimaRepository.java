9a2e056ff0d468b13297fb5a667d83d1b0297601
FCosca <65341207+FCosca@users.noreply.github.com>
2020-07-10 00:33:41 +0200

    progetto Sistemi

---
+++ src/main/java/web/backtospring/repositories/MateriaPrimaRepository.java
@@ -0,0 +1,7 @@
+package web.backtospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import web.backtospring.entities.MateriaPrima;
+
+public interface MateriaPrimaRepository extends JpaRepository<MateriaPrima, Integer> {
+}
