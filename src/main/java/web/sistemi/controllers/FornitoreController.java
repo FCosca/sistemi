76d736abd8e07fefe3f83add5272c638f17bdcee
FCosca <65341207+FCosca@users.noreply.github.com>
2020-07-10 20:05:54 +0200

    progetto Sistemi

--- src/main/java/web/sistemi/controllers/FornitoreController.java
+++ src/main/java/web/sistemi/controllers/FornitoreController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import web.sistemi.entities.Fornitore;
 import web.sistemi.services.FornitoreService;
 import web.sistemi.supporto.PivaGiaEsistenteException;
@@ -19,11 +16,13 @@ public class FornitoreController {
     @Autowired
     private FornitoreService fornitoreService;
 
+    @RequestMapping("/fornitori")
     @GetMapping("/fornitori")
     public ResponseEntity getAll() {
         return new ResponseEntity<>(fornitoreService.allFornitore(), HttpStatus.OK);
     }//getAll
 
+    @RequestMapping("/fornitori/{PIVA}")
     @GetMapping("/fornitori/{PIVA}")
     public ResponseEntity getByPiva(@PathVariable("PIVA") String PIVA){
         Fornitore fornitore = fornitoreService.getByPIva(PIVA);
@@ -32,6 +31,7 @@ public class FornitoreController {
         }//if
         return new ResponseEntity<>(fornitore, HttpStatus.OK);
     }//getByPiva
+
 
     @PostMapping("/fornitori")
     public ResponseEntity create(@RequestBody Fornitore fornitore) throws PivaGiaEsistenteException{
